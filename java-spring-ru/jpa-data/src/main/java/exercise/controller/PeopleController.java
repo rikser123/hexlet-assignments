a5796b246f1d2bb4298db24a842279d15b9473f3
@hexlet/cli <support@hexlet.io>
2024-09-21 16:03:36 +0300

    submit java-spring-ru/jpa-data

---
+++ java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
@@ -0,0 +1,51 @@
+package exercise.controller;
+
+import exercise.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+import exercise.model.Person;
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @GetMapping(path = "/{id}")
+    public Person show(@PathVariable long id) {
+        return personRepository.findById(id).get();
+    }
+
+    // BEGIN
+    @GetMapping()
+    public List<Person> getPeoples() {
+        var peoples = personRepository.findAll();
+
+        return peoples;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePerson(@PathVariable Long id) {
+        personRepository.deleteById(id);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Person createPerson(@RequestBody Person person) {
+        var newPerson = personRepository.save(person);
+        return newPerson;
+    }
+    // END
+}
