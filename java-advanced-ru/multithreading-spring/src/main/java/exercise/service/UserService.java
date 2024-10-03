bd4031606fbd27b1404faaaead82cf56fdc385a7
@hexlet/cli <support@hexlet.io>
2024-10-03 12:39:13 +0300

    submit java-advanced-ru/multithreading-spring

---
+++ java-advanced-ru/multithreading-spring/src/main/java/exercise/service/UserService.java
@@ -0,0 +1,40 @@
+package exercise.service;
+
+import exercise.model.User;
+import exercise.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public Flux<User> findAll() {
+        return userRepository.findAll();
+    }
+
+
+    // BEGIN
+    public Mono<User> getById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Mono<User> create(User user) {
+        return userRepository.save(user);
+    }
+
+    public Mono<User> update(Long id, User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+    // END
+}
